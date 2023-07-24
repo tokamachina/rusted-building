f2fbf6bfb44ff74672c40197ba2ca019cd7621ca
tokamachina <tokamachina@gmail.com>
2023-07-24 14:19:40 -0500

    serializing and deserializing ggpk_specs (json)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use rlua::{Lua, Result, Table};
-use std::collections::HashMap;
+use rlua::{Error, Lua, Result, Table};
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::path::Path;
 
@@ -14,26 +15,55 @@ fn main() -> Result<()> {
             .call::<_, Table>(())?
             .pairs::<String, Table>();
         let mut specs: HashMap<String, Vec<GGPKColumn>> = HashMap::new();
+        let mut types_set: HashSet<String> = HashSet::new();
         for pair in spectables {
             let (key, value) = pair?;
             if value.len()? != 0 {
                 let col_spec: Vec<GGPKColumn> = value
                     .sequence_values::<Table>()
-                    .map(|x| x.expect("ggpk table sequence").into())
+                    .map(|x| x.expect("ggpkseq").into())
                     .collect();
-                specs.insert(key.to_string(), col_spec);
-                println!("{:?} added to specmap", key);
+                for col in &col_spec[..] {
+                    types_set.insert(col.column_type.clone());
+                }
+                println!("{:?} added to specmap", &key);
+                specs.insert(key, col_spec);
             }
         }
-        Ok(())
-    })
+        println!("Types: {:?}", &types_set);
+        let jsonwriter = fs::File::create(Path::new("ggpk_specs.json")).expect("fileopen");
+        serde_json::to_writer(jsonwriter, &specs).expect("ser fail");
+        Ok::<(), Error>(())
+    })?;
+
+    let jsonreader = &fs::File::open(Path::new("ggpk_specs.json")).expect("fileopen");
+    let _specs: HashMap<String, Vec<GGPKColumn>> =
+        serde_json::from_reader(jsonreader).expect("ser fail");
+
+    Ok(())
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 struct GGPKColumn {
     name: String,
     width: u32,
     column_type: String,
+    column_is_list: bool,
     ref_to: Option<String>,
+    data: Option<GGPKColumnData>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum GGPKColumnData {
+    GGPKFloat(Vec<f64>),
+    GGPKInterval(Vec<(i32, i32)>),
+    GGPKKey(Vec<Vec<u64>>),
+    GGPKInt(Vec<i32>),
+    GGPKShortKey(Vec<u32>),
+    GGPKBool(Vec<bool>),
+    GGPKString(Vec<String>),
+    GGPKEnum(Vec<u32>),
+    GGPKUInt(Vec<u32>),
 }
 
 impl From<Table<'_>> for GGPKColumn {
@@ -42,7 +72,9 @@ impl From<Table<'_>> for GGPKColumn {
             name: s.get("name").expect("bad name"),
             width: s.get("width").expect("bad width"),
             column_type: s.get("type").expect("bad column_type"),
+            column_is_list: s.get("list").expect("bad islist"),
             ref_to: s.get("refTo").expect("bad refTo"),
+            data: None,
         }
     }
 }
